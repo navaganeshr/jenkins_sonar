d3f37271c605c28cc5e7dc9d94480be8ee2a7381
Navaganesh Raju <48936764+navaganeshr@users.noreply.github.com>
2020-01-05 11:16:18 +0530

    Update emailgen.sh

--- emailgen.sh
+++ emailgen.sh
@@ -4,5 +4,5 @@ PROJECT_ARCHIVE_NAME=$2
 COMMIT_ID=$3
 
 cat <<-EOF >> ${WORKSPACE}/email.html
-<div face="verdana" style = "color:blue"><br/>Hi,<br/><br/><B>Jenkins Build Success !!!!!!! </B><br/><br/><br/><br/>Please find the job details below:<br/><br/><br/><ul> <li><B>TAG: </B> ${VERSION} </li><li><B>ARTIFACT: </B> VFQA_SCMS_UIM_Cartridge-${VERSION}.jar</li><li><B>BRANCH NAME : </B> ${PROJECT_ARCHIVE_NAME} </li><li><B>COMMIT_ID: </B> ${COMMIT_ID}</li><li><B>JOB_NAME: </B> ${JOB_NAME}</li><li><B>BUILD NUMBER:</B> ${BUILD_NUMBER}</li><li><B>More info at: </B>${JOB_URL}</li></ul><br/><br/>Please find the build log attached<br/></div>
+<div face="verdana" style = "color:#0F0145 "><br/>Hi,<br/><br/><B>Jenkins Build Success !!!!!!! </B><br/><br/><br/><br/>Please find the job details below:<br/><br/><br/><ul> <li><B>TAG: </B> ${VERSION} </li><li><B>ARTIFACT: </B> VFQA_SCMS_UIM_Cartridge-${VERSION}.jar</li><li><B>BRANCH NAME : </B> ${PROJECT_ARCHIVE_NAME} </li><li><B>COMMIT_ID: </B> ${COMMIT_ID}</li><li><B>JOB_NAME: </B> ${JOB_NAME}</li><li><B>BUILD NUMBER:</B> ${BUILD_NUMBER}</li><li><B>More info at: </B>${JOB_URL}</li></ul><br/><br/>Please find the build log attached<br/></div>
 	EOF
